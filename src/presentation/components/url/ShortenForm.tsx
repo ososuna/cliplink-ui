5fb8ed689758060b2226b4e703f0c4062ebcfb5e
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-11-30 15:37:47 -0600

    use shadcn from

--- src/presentation/components/url/ShortenForm.tsx
+++ src/presentation/components/url/ShortenForm.tsx
@@ -1,51 +1,71 @@
 import { useState } from 'react';
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
 import { Loader2 } from 'lucide-react';
+
 import { Button } from '../ui/button';
 import { CardContent, CardFooter } from '../ui/card';
-import { Label } from '../ui/label';
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form';
 import { Input } from '../ui/input';
 
-import { useForm } from '@/infrastructure/hooks/useForm';
+const formSchema = z.object({
+  longUrl: z.string().url()
+});
 
 const ShortenForm = () => {
+
   const [ isLoading, setIsLoading ] = useState(false);
-  const { form, onChange } = useForm({ longUrl: '' });
-  const { longUrl } = form;
   
-  const handleClick = () => {
+   const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      longUrl: '',
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    const { longUrl } = values;
+    console.log(longUrl);
     setIsLoading(true);
-    // todo: http call
-    window.location.href = '/short/asd';
-    setIsLoading(false);
-  };
+  }
 
   return (
     <>
-      <CardContent>
-        <div className="grid w-full items-center gap-4">
-          <div className="flex flex-col space-y-1.5">          
-            <Label htmlFor="longUrl">Long URL</Label>
-            <Input
-              type="text"
-              autoComplete="off"
-              id="longUrl"
-              placeholder="https://example.com/very/long/url"
-              required
-              value={ longUrl }
-              onChange={ (event) => onChange(event, 'longUrl') }
-            />
-          </div>
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button onClick={ handleClick } type="button">
-          { isLoading ? (
-            <><Loader2 className="animate-spin" /> Loading</>
-          ) : (
-            'Shorten URL'
-          )}
-        </Button>
-      </CardFooter>
+      <Form {...form}>
+        <form onSubmit={ form.handleSubmit(onSubmit) }>
+          <CardContent>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col">          
+                <FormField
+                  control={form.control}
+                  name="longUrl"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Long URL</FormLabel>
+                      <FormControl>
+                        <Input placeholder="https://example/long/url" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit">
+              { isLoading ? (
+                <><Loader2 className="animate-spin" /> Shortening...</>
+              ) : (
+                'Shorten URL'
+              )}
+            </Button>
+          </CardFooter>
+        </form>
+      </Form>
     </>
   );
 };
