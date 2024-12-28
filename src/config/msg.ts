7315db0160a82397f9f4c2f8bc63e461c1c7cf1d
Oswaldo Adrian Osuna <oswaldo.adrian@live.com.mx>
2024-12-27 20:27:44 -0700

    SEO changes

--- src/config/msg.ts
+++ src/config/msg.ts
@@ -6,4 +6,5 @@ export const Messages = {
   STRING_MAX: (field: string, max: number) => `The ${field} must be no more than ${max} character(s) long`,
   VALID_URL: 'Enter a valid URL.',
   INTERNAL_SERVER_ERROR: 'Something went wrong. Please try again later. If the problem persists, contact support.',
+  APP_DESCRIPTION: 'ClipLink - The ultimate URL shortener for seamless link management. Shorten, customize, and track your links with ease. Boost your branding and analytics with a fast, reliable, and user-friendly solution. Try ClipLink today!',
 };
