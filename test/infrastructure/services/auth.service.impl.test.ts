cd529a59af44f6f7df03c45e6be773c0620ffa42
Oswaldo Osuna <oswaldo.adrian@live.com.mx>
2025-06-18 19:37:25 -0600

    test: login by email test cases

--- test/infrastructure/services/auth.service.impl.test.ts
+++ test/infrastructure/services/auth.service.impl.test.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeEach, vi } from 'vitest';
-import { type AuthRepository, type AuthService } from '@/domain';
+import { CustomError, type AuthRepository, type AuthService } from '@/domain';
 import { AuthServiceImpl } from '@/infrastructure';
+import { Messages } from '@/config';
 import { AuthServiceMocks } from '@test/test-utils/infrastructure/services/auth.service.mocks';
 
 describe('AuthServiceImpl', () => {
@@ -24,6 +25,33 @@ describe('AuthServiceImpl', () => {
       expect(mockAuthRepository.login).toHaveBeenCalledWith({
         email: mockUser.email,
         password: '12345678'
+      });
+    });
+
+    it('should notify an error if the user is not found', async () => {
+      const mockUser = AuthServiceMocks.user;
+      vi.mocked(mockAuthRepository.login).mockRejectedValue(CustomError.badRequest(Messages.BAD_CREDENTIALS));
+      await authService.loginByEmail(mockUser.email, '12345678');
+      expect(notifyUiErrorMock).toHaveBeenCalledWith({
+        type: 'error',
+        message: Messages.BAD_CREDENTIALS
+      });
+    });
+
+    it('should notify missing password error', async () => {
+      const mockUser = AuthServiceMocks.user;
+      await authService.loginByEmail(mockUser.email, '');
+      expect(notifyUiErrorMock).toHaveBeenCalledWith({
+        type: 'error',
+        message: Messages.PASSWORD_REQUIRED
+      });
+    });
+
+    it('should notify missing email error', async () => {
+      await authService.loginByEmail('', '12345678');
+      expect(notifyUiErrorMock).toHaveBeenCalledWith({
+        type: 'error',
+        message: Messages.MISSING_EMAIL
       });
     });
   });
